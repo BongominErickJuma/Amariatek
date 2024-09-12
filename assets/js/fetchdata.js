63074591ca5cc9fd299374497eb2888ca80ef48b
BongominErickJuma <104322246+BongominErickJuma@users.noreply.github.com>
2024-09-12 10:51:59 +0300

    Update fetchdata.js

--- assets/js/fetchdata.js
+++ assets/js/fetchdata.js
@@ -2,7 +2,6 @@ const windowURL = new URL(window.location.href);
 const params = new URLSearchParams(windowURL.search);
 const id = parseInt(params.get("id"));
 
-alert("Hello")
 
 fetch("https://bongominerickjuma.github.io/Amariatek/assets/json/data.json")
   .then((response) => response.json()) // Parse the JSON file into a JavaScript object
